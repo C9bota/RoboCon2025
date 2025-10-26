e876ba42b451caec0f6d7be37785012b55a64289
switch23 <switch153r81953@icloud.com>
2025-10-26 18:56:19 +0900

    インターフェースを仮置き

--- Prototype/Masuda/include/servo_move.h
+++ Prototype/Masuda/include/servo_move.h
@@ -1,13 +1,25 @@
 # ifdef SERVO_MOVE_H
 # define SERVO_MOVE_H
 
+#include "servo_channel.h"
 #include "servo_command.h"
 
-struct ServoCommandInfo {
-    SERVO_COMMAND command;
-    int send_buffer_size;
-    int receive_buffer_size;
-};
+// サーボモータ動作モード設定
+public void SetServoSyncMode(int mode);
+
+// サーボモータ一斉スタート
+public void StartServoSync();
+
+// PWMパルス出力許可
+public void AllowPwmPulseOutput(SERVO_CHANNEL channel, bool allow);
+
+// サーボモータ目標位置設定（角度指定）
+public void SetServoTargetByAngle(SERVO_CHANNEL channel, int ANGLE);
+
+// サーボモータ現在位置取得（角度指定）
+public int GetServoPositionByAngle(SERVO_CHANNEL channel);
+
+
 
 
 #endif
