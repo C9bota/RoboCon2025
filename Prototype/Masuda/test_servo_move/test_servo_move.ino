16a22295d7bb452df6c9b0cd5895a069c8ab3bc8
switch23 <switch153r81935@icloud.com>
2025-11-09 01:31:37 +0900

    モーターのテストを修正

--- Prototype/Masuda/test_servo_move/test_servo_move.ino
+++ Prototype/Masuda/test_servo_move/test_servo_move.ino
@@ -1,38 +1,78 @@
 #include <Wire.h>
 #include "./lib/servo_command.h"
 #include "./lib/servo_move.h"
-//#include "./src/servo_move.cpp"
 
 void setup() {
     Serial.begin(115200);
+    Wire.setClock(100000);
     Wire.begin();
     delay(1000);
     Serial.println("[TEST] servo_move.cpp 関数テスト開始");
 
-    // サーボ動作モード設定
+    // サーボモータ動作モード設定
     SetServoSyncMode(SYNC_MODE);
     Serial.println("SetServoSyncMode(SYNC_MODE) 呼び出しOK");
 
-    // サーボ一斉スタート
-    StartServoSync();
-    Serial.println("StartServoSync() 呼び出しOK");
-
-    // PWMパルス出力許可
-    AllowPwmPulseOutput(RIGHT_HAND_CHANNEL, ENABLE);
-    Serial.println("AllowPwmPulseOutput(RIGHT_HAND_CHANNEL, ENABLE) 呼び出しOK");
-
-    // サーボ目標位置設定（角度指定）
-    SetServoTargetByAngle(RIGHT_HAND_CHANNEL, 900, 500); // 90.0度, 500ms
-    Serial.println("SetServoTargetByAngle(RIGHT_HAND_CHANNEL, 900, 500) 呼び出しOK");
-
-    // サーボ現在位置取得（角度指定）
-    int pos = GetServoPositionByAngle(RIGHT_HAND_CHANNEL);
-    Serial.print("GetServoPositionByAngle(RIGHT_HAND_CHANNEL) = ");
-    Serial.println(pos);
-
-    Serial.println("[TEST] 完了");
+    // PWM パルス出力許可
+    AllowPwmPulseOutput(LEFT_HAND_CHANNEL, ENABLE);
+    Serial.println("AllowPwmPulseOutput(LEFT_HAND_CHANNEL, ENABLE) 呼び出しOK");
+    delay(100);
 }
 
 void loop() {
-	// 何もしない
+    static int phase = 0;
+    static int j = 0;
+    int pos;
+    switch (phase%5) {
+        case 0:
+            // モータの角度を0度にセット
+            SetServoTargetByAngle(LEFT_HAND_CHANNEL, 0, 1000);
+            Serial.println("SetServoTargetByAngle(LEFT_HAND_CHANNEL, 0, 1000)");
+            StartServoSync();
+            Serial.println("StartServoSync()");
+            delay(1000);
+            phase++;
+            break;
+        case 1:
+            // モータの現在座標を取得
+            pos = GetServoPositionByAngle(LEFT_HAND_CHANNEL);
+            Serial.print("Angle: ");
+            Serial.println(pos);
+            phase++;
+            break;
+        case 2:
+            // モータの角度を180度にセット
+            SetServoTargetByAngle(LEFT_HAND_CHANNEL, 1800, 1000);
+            Serial.println("SetServoTargetByAngle(LEFT_HAND_CHANNEL, 1800, 1000)");
+            StartServoSync();
+            Serial.println("StartServoSync()");
+            j = 0;
+            phase++;
+            break;
+        case 3:
+            if (j < 10) {
+                delay(100);
+                pos = GetServoPositionByAngle(LEFT_HAND_CHANNEL);
+                Serial.print("Angle: ");
+                Serial.println(pos);
+                j++;
+            } else {
+                phase++;
+            }
+            break;
+        case 4:
+            delay(400);
+            // モータの角度を90度にセット
+            SetServoTargetByAngle(LEFT_HAND_CHANNEL, 900, 1000);
+            Serial.println("SetServoTargetByAngle(LEFT_HAND_CHANNEL, 900, 1000)");
+            StartServoSync();
+            Serial.println("StartServoSync()");
+            delay(1000);
+            phase++;
+            break;
+		default:
+            // 停止
+            break;
+    }
+    delay(100);
 }
